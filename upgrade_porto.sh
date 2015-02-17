ff3b2fc4fdce3a8a3e7cb076c3d1723c5deedd7f
Chris Custine <ccustine@apache.org>
2015-02-16 19:02:10 -0700

    Convert default pages for Jeckyll

--- upgrade_porto.sh
+++ upgrade_porto.sh
@@ -28,3 +28,13 @@ cp custom/tmp/css/custom.scss.bak $newdir/custom.scss;
 
 # Can't remember what I was using this for
 #echo "\n@import \"variables_overrides\";" >> master/sass/config/_imports.scss;
+
+
+# These can be run to strip the standard headers and footers
+# Still requires manual addition of front matter and removing an extra </div> at the end for now
+
+# Strip footers first
+# matches=$( ack -m 1 -o "<footer" --type=html --ignore-dir=_includes --ignore-dir=_layouts|cut -d: -f1,2 ); while read -r match; do file=$( echo $match | cut -d: -f1 ) ; line=$( echo $match | cut -d: -f2 ) ; sed -i -e "${line},\$d" $file; done <<< "$matches"
+
+# Then strip headers
+# matches=$( ack -m 1 -o "<div role=\"main\" class=\"main\">" --type=html --ignore-dir=_includes --ignore-dir=_layouts|cut -d: -f1,2 ); while read -r match; do file=$( echo $match | cut -d: -f1 ) ; line=$( echo $match | cut -d: -f2 ) ; sed -i -e "1,${line}d" $file; done <<< "$matches"
